bd7a9dde2afd92e86288f20b7be1272eb243feb2
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2022-11-10 22:44:13 +0300

    project updated

--- src/test/java/step_definitions/ExcelReadData.java
+++ src/test/java/step_definitions/ExcelReadData.java
@@ -21,5 +21,30 @@ public class ExcelReadData {
 
     }
 
+    @Test
+    public void writeData() throws IOException {
+        String path = "sample.xlsx";
+        FileInputStream fileInputStream = new FileInputStream(path);
+        XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+        XSSFSheet sheet = workbook.getSheet("Finance");
+
+        XSSFCell date = sheet.getRow(0).createCell(3);
+        date.setCellValue("Date");
+
+        XSSFCell date1 = sheet.getRow(1).createCell(3);
+        date1.setCellValue("15/06");
+
+        XSSFCell date2 = sheet.getRow(2).createCell(3);
+        date2.setCellValue("14/12");
+
+        FileOutputStream fileOutputStream = new FileOutputStream(path);
+        workbook.write(fileOutputStream);
+
+        fileOutputStream.close();
+        workbook.close();
+        fileInputStream.close();
+
+    }
+
 
 }
